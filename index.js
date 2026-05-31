8b4467e6cd5ca868a26e99be85109b2b2b5adcf2
XhmikosR <xhmikosr@gmail.com>
2026-05-31 19:31:18 +0300

    index.js: use systemArchitectureSync directly

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import process from 'node:process';
-import {systemArchitectureSync as arch} from 'system-architecture';
+import {systemArchitectureSync} from 'system-architecture';
 
 const matchesFilter = (shouldCheck, expected, actual) => {
 	return !shouldCheck || !expected || expected === actual;
@@ -7,7 +7,7 @@ const matchesFilter = (shouldCheck, expected, actual) => {
 
 const osFilterObj = input => {
 	return input.filter(item => {
-		return [process.platform, arch()].every((sysValue, index) => {
+		return [process.platform, systemArchitectureSync()].every((sysValue, index) => {
 			return matchesFilter(index === 0, item.os, sysValue)
 				&& matchesFilter(index === 1, item.arch, sysValue);
 		});
