307fe9abafabf76917181325252b865f19236b8d
SukeshP1995 <30713956+SukeshP1995@users.noreply.github.com>
2026-07-01 22:14:17 +0200

    update matrix for CI (#2)
    * update matrix for CI
    
    * feat: add test for checking the architecture
    
    * chore: skip test if not running in CI
    
    ---------
    
    Co-authored-by: XhmikosR <xhmikosr@gmail.com>

--- test.js
+++ test.js
@@ -23,3 +23,20 @@ test('works', t => {
 	t.is(osFilterObj(fixtures)[0].os, undefined);
 	t.is(osFilterObj(fixtures)[1].os, process.platform);
 });
+
+// This test can be checked only if RUNNER_ARCH is set
+(process.env.CI ? test : test.skip)('should filter expected architecture', t => {
+	const fixtures = [{
+		foo: 'all',
+	}, {
+		foo: 'x64',
+		arch: 'x64',
+	}, {
+		foo: 'arm64',
+		arch: 'arm64',
+	}];
+
+	t.is(osFilterObj(fixtures).length, 2);
+	t.is(osFilterObj(fixtures)[0].os, undefined);
+	t.is(osFilterObj(fixtures)[1].arch, process.env.RUNNER_ARCH?.toLocaleLowerCase());
+});
